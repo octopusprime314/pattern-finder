2ee6866b6f4cde4d54ec13d428030767beef710c
octopusprime314 <peter_morley@my.uri.edu>
2017-04-03 22:40:29 -0400

    More fixes
    making sure folders are in place before writing files.

--- scripts/testScript.bat
+++ scripts/testScript.bat
@@ -1,3 +1,2 @@
-start "" /WAIT "../bin/PatternFinder.exe" -f Data -v 1 -c -h
-start "" /WAIT "../bin/PatternFinder.exe" -f Data -v 1 -c -ram
-start "" /WAIT "../bin/PatternFinder.exe" -f Data -v 1 -c -mem 100
+start "" /WAIT "../bin/Release/PatternFinder.exe" -f Data -v 1 -c -hd
+start "" /WAIT "../bin/Release/PatternFinder.exe" -f Data -v 1 -c -ram
