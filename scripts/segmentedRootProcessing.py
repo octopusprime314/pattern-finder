5304dda627178e7f4c51c41c425f1385e0ddb827
Peter Morley <pmorley@ele.uri.edu>
2017-04-23 07:48:00 -0400

    Segment Processor can give each job a thread count now

--- scripts/segmentedRootProcessing.py
+++ scripts/segmentedRootProcessing.py
@@ -6,6 +6,7 @@ import subprocess
 
 fileName = sys.argv[1]
 jobs = int(sys.argv[2])
+threadsperjob = int(sys.argv[3])
 file = open(fileName, "rb")
 fileData = file.read()
 file.close()
@@ -27,7 +28,7 @@ leftrange = 0
 division =  256/jobs
 counter = 0
 while counter < jobs:
-	cmdargs = ['./PatternFinder', '-f', '../' + fileName , '-threads', '1', '-v', '1', '-lr', str(leftrange), '-hr', str(leftrange + division - 1), '-ram']
+	cmdargs = ['./PatternFinder', '-f', '../' + fileName , '-threads', str(threadsperjob), '-v', '1', '-lr', str(leftrange), '-hr', str(leftrange + division - 1), '-ram']
 	print cmdargs
 	proc = subprocess.Popen(cmdargs)
 	leftrange = leftrange + division
