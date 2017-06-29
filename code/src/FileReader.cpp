0f5996d38f400805acec0714704cba27f7f744b8
octopusprime314 <peter_morley@my.uri.edu>
2017-06-29 02:47:40 -0400

    Added copyrights
    bleh

--- code/src/FileReader.cpp
+++ code/src/FileReader.cpp
@@ -1,3 +1,34 @@
+/*
+ * Copyright (c) 2016, 2017
+* The University of Rhode Island
+ * All Rights Reserved
+ *
+ * This code is part of the Pattern Finder.
+ *
+ * Permission is granted to use, copy, create derivative works and
+ * redistribute this software and such derivative works for any
+ * purpose, so long as the copyright notice above, this grant of
+ * permission, and the disclaimer below appear in all copies made; and
+ * so long as the name of The University of Rhode Island is not used in
+ * any advertising or publicity pertaining to the use or distribution
+ * of this software without specific, written prior authorization.
+ *
+ * THIS SOFTWARE IS PROVIDED AS IS, WITHOUT REPRESENTATION FROM THE
+ * UNIVERSITY OF RHODE ISLAND AS TO ITS FITNESS FOR ANY PURPOSE, AND
+ * WITHOUT WARRANTY BY THE UNIVERSITY OF RHODE ISLAND OF ANY KIND, EITHER
+ * EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE. THE UNIVERSITY OF RHODE ISLAND SHALL NOT BE
+ * LIABLE FOR ANY DAMAGES, INCLUDING DIRECT, SPECIAL, INDIRECT,
+ * INCIDENTAL, OR CONSEQUENTIAL DAMAGES, WITH RESPECT TO ANY CLAIM
+ * ARISING OUT OF OR IN CONNECTION WITH THE USE OF THE SOFTWARE, EVEN
+ * IF IT HAS BEEN OR IS HEREAFTER ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGES.
+ *
+ * Author: Peter J. Morley 
+ *          
+ */
+
 #include "FileReader.h"
 #include <sstream>
 #include <iostream>
